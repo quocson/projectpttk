9f5d722606f562c06021c25e7d4a15edfc8e55eb
darklord.nlqs <darklord.nlqs@gmail.com>
2011-12-27 21:54:19 +0000

    Finish ver1 user control

--- TetrisReturn/TetrisReturn/MainForm.Designer.cs
+++ TetrisReturn/TetrisReturn/MainForm.Designer.cs
@@ -29,13 +29,34 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.showInformation1 = new TetrisReturn.ShowInformation();
             this.SuspendLayout();
+            // 
+            // showInformation1
+            // 
+            this.showInformation1.CNumber = System.Drawing.Color.Empty;
+            this.showInformation1.CStroke = System.Drawing.Color.Black;
+            this.showInformation1.CText = System.Drawing.Color.White;
+            this.showInformation1.Drawabled = false;
+            this.showInformation1.FNumber = null;
+            this.showInformation1.FText = null;
+            this.showInformation1.ImgBack = null;
+            this.showInformation1.IWidth = 2;
+            this.showInformation1.Location = new System.Drawing.Point(151, 46);
+            this.showInformation1.Name = "showInformation1";
+            this.showInformation1.Number = 0;
+            this.showInformation1.PNumber = new System.Drawing.Point(0, 0);
+            this.showInformation1.PText = new System.Drawing.Point(0, 0);
+            this.showInformation1.Size = new System.Drawing.Size(173, 165);
+            this.showInformation1.SText = null;
+            this.showInformation1.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(116, 0);
+            this.ClientSize = new System.Drawing.Size(659, 251);
+            this.Controls.Add(this.showInformation1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "Tetris Return";
@@ -44,6 +65,8 @@
         }
 
         #endregion
+
+        private ShowInformation showInformation1;
 
 
 
--- TetrisReturn/TetrisReturn/ShowInformation.cs
+++ TetrisReturn/TetrisReturn/ShowInformation.cs
@@ -6,6 +6,8 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 
 namespace TetrisReturn
 {
@@ -13,38 +15,38 @@ namespace TetrisReturn
     {
         //title 
         private String sText;// button text
-        public System.String SText
+        public String SText
         {
             get { return sText; }
             set { sText = value; }
         }
-        private Color tColor = Color.White; // text color
-        public System.Drawing.Color TColor
+        private Color cText = Color.White; // text color
+        public Color CText
         {
-            get { return tColor; }
-            set { tColor = value; }
+            get { return cText; }
+            set { cText = value; }
         }
-        private Color strokeColor = Color.Black; //stroke color
-        public System.Drawing.Color StrokeColor
+        private Color cStroke = Color.Black; //stroke color
+        public Color CStroke
         {
-            get { return strokeColor; }
-            set { strokeColor = value; }
+            get { return cStroke; }
+            set { cStroke = value; }
         }
-        private int strokeWidth = 2; //stroke width
+        private int iWidth = 2; //stroke width
 
-        public int StrokeWidth
+        public int IWidth
         {
-            get { return strokeWidth; }
-            set { strokeWidth = value; }
+            get { return iWidth; }
+            set { iWidth = value; }
         }
         private Font fText; // font of text
-        public System.Drawing.Font FText
+        public Font FText
         {
             get { return fText; }
             set { fText = value; }
         }
         private Point pText; //pos of text
-        public System.Drawing.Point PText
+        public Point PText
         {
             get { return pText; }
             set { pText = value; }
@@ -69,38 +71,142 @@ namespace TetrisReturn
             set { fNumber = value; }
         }
         private Color cNumber;
-        public System.Drawing.Color CNumber
+        public Color CNumber
         {
             get { return cNumber; }
             set { cNumber = value; }
         }
         //Image
-        private Bitmap image;
-        public System.Drawing.Bitmap Image
+        private Bitmap imgBack;
+
+        public Bitmap ImgBack
         {
-            get { return image; }
-            set { image = value; }
+            get { return imgBack; }
+            set { imgBack = value; }
+        }
+  
+        private bool drawabled;
+        public bool Drawabled
+        {
+            get { return drawabled; }
+            set
+            {
+                drawabled = value;
+                if (!Drawabled)
+                    return;
+
+                SizeF sz = Graphics.FromImage(new Bitmap(2, 2)).MeasureString(SText, FText);
+                if (Width - (int)sz.Width < 0)
+                    PText = new Point(0, 0);
+                else
+                    PText = new Point((Width - (int)sz.Width) / 2, 0);
+                Refresh();
+            }
         }
         public ShowInformation()
         {
             InitializeComponent();
+            Drawabled = false;
         }
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            if (!Enabled)
+            if (!Drawabled)
                 return;
-            Font TFont = new Font("Transformers Movie", 20, FontStyle.Bold);
-            Point TPos;
-            SizeF sz = Graphics.FromImage(new Bitmap(2, 2)).MeasureString(SText, TFont);
-            picBox.Top = (int)sz.Height;
-            //sap xep vi tri cua text
-            if (picBox.Height - (int)sz.Height < 0 || picBox.Width - (int)sz.Width < 0)
-                TPos = new Point(0, 0);
+            SizeF sz = Graphics.FromImage(new Bitmap(2, 2)).MeasureString(SText, FText);
+            //sap xep vi tri cua title
+            if (Width - (int)sz.Width < 0)
+                PText = new Point(0, 0);
             else
-                TPos = new Point((picBox.Width - (int)sz.Width) / 2, 0);
-            e.Graphics.DrawString("Next Shape", TFont, new SolidBrush(Color.Red), TPos);
-            //Graphics.FromImage(picBox.Image)
+                PText = new Point((Width - (int)sz.Width) / 2, 0);
+            SizeF szn = Graphics.FromImage(new Bitmap(2, 2)).MeasureString(Number.ToString(), FNumber);
+            if (Width - (int)szn.Width < 0)
+                PNumber= new Point(0, 0);
+            else
+                PNumber = new Point((Width - (int)szn.Width) / 2, (int)sz.Width);
+            e.Graphics.DrawImage(ImgBack, new Point(0, 0));
+            e.Graphics.DrawImage(getImgFromTxt(), PText);
+            e.Graphics.DrawImage(getImgFromNo(), PNumber);
+        }
+        private Image getImgFromTxt()
+        {
+            Bitmap bmpOut = null; // bitmap we are creating and will return from this function.
+            if (FText == null || SText == null)
+                return bmpOut;
+            using (Graphics g = Graphics.FromHwnd(IntPtr.Zero))
+            {
+                SizeF sz = g.MeasureString(SText, FText);
+                Bitmap bmp = new Bitmap((int)sz.Width, (int)sz.Height);
+                Graphics gBmp = Graphics.FromImage(bmp);
+                SolidBrush brBack = new SolidBrush(Color.FromArgb(255, CStroke.R, CStroke.G, CStroke.B));
+                using (SolidBrush brFore = new SolidBrush(CText))
+                {
+                    gBmp.SmoothingMode = SmoothingMode.HighQuality;
+                    gBmp.InterpolationMode = InterpolationMode.HighQualityBilinear;
+                    gBmp.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+
+                    gBmp.DrawString(SText, FText, brBack, 0, 0);
+
+                    // make bitmap we will return.
+                    bmpOut = new Bitmap(bmp.Width + iWidth, bmp.Height + iWidth);
+                    using (Graphics gBmpOut = Graphics.FromImage(bmpOut))
+                    {
+                        gBmpOut.SmoothingMode = SmoothingMode.HighQuality;
+                        gBmpOut.InterpolationMode = InterpolationMode.HighQualityBilinear;
+                        gBmpOut.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+
+                        // smear image of background of text about to make blurred background "halo"
+                        for (int x = 0; x <= iWidth; x++)
+                            for (int y = 0; y <= iWidth; y++)
+                                gBmpOut.DrawImageUnscaled(bmp, x, y);
+
+                        // draw actual text
+                        gBmpOut.DrawString(SText, FText, brFore, iWidth / 2, iWidth / 2);
+                    }
+                }
+            }
+
+            return bmpOut;
+        }
+        private Image getImgFromNo()
+        {
+            Bitmap bmpOut = null; // bitmap we are creating and will return from this function.
+            if (FNumber == null || Number.ToString() == null)
+                return bmpOut;
+            using (Graphics g = Graphics.FromHwnd(IntPtr.Zero))
+            {
+                SizeF sz = g.MeasureString(Number.ToString(), FNumber);
+                Bitmap bmp = new Bitmap((int)sz.Width, (int)sz.Height);
+                Graphics gBmp = Graphics.FromImage(bmp);
+                SolidBrush brBack = new SolidBrush(Color.FromArgb(255, CStroke.R, CStroke.G, CStroke.B));
+                using (SolidBrush brFore = new SolidBrush(CNumber))
+                {
+                    gBmp.SmoothingMode = SmoothingMode.HighQuality;
+                    gBmp.InterpolationMode = InterpolationMode.HighQualityBilinear;
+                    gBmp.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+
+                    gBmp.DrawString(Number.ToString(), FNumber, brBack, 0, 0);
+
+                    // make bitmap we will return.
+                    bmpOut = new Bitmap(bmp.Width + iWidth, bmp.Height + iWidth);
+                    using (Graphics gBmpOut = Graphics.FromImage(bmpOut))
+                    {
+                        gBmpOut.SmoothingMode = SmoothingMode.HighQuality;
+                        gBmpOut.InterpolationMode = InterpolationMode.HighQualityBilinear;
+                        gBmpOut.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+
+                        // smear image of background of text about to make blurred background "halo"
+                        for (int x = 0; x <= iWidth; x++)
+                            for (int y = 0; y <= iWidth; y++)
+                                gBmpOut.DrawImageUnscaled(bmp, x, y);
+
+                        // draw actual text
+                        gBmpOut.DrawString(Number.ToString(), FNumber, brFore, iWidth / 2, iWidth / 2);
+                    }
+                }
+            }
+
+            return bmpOut;
         }
     }
 }
